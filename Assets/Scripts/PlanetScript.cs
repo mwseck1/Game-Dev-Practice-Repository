d578520b31186011b20a83bbf9cb2f596a80f339
Michael Seck <michaelseck7@gmail.com>
2020-02-19 16:45:57 -0600

    Added pop up window for planetary information
    Added a pop up window, will give planetary information, and allow for the creation of player ships eventually.

--- Assets/Scripts/PlanetScript.cs
+++ Assets/Scripts/PlanetScript.cs
@@ -5,7 +5,20 @@ using UnityEngine;
 [System.Serializable]
 public class PlanetScript : Sprite
 {
-   
+   public Rect popUpWindow;
+   public Vector3 coordinates;
+
+   public void CreateWindow(int WindowID)
+   {
+
+   }
+
+   public void OnGUI()
+   {
+        if(isSelected)
+            popUpWindow = GUI.Window(0, popUpWindow, CreateWindow, "meow");
+   }
+
    public void OnMouseOver()
     {
         if(Input.GetMouseButtonDown(1) && !isSelected && GameObject.Find("p1").GetComponent<P1Controller>().isSelected == true)
@@ -18,19 +31,25 @@ public class PlanetScript : Sprite
     public override void Start()
     {
         transform.GetChild(0).GetComponent<Renderer>().enabled = false;
+        coordinates = new Vector3(transform.position.x, transform.position.y,1.0f);
+        coordinates = RectTransformUtility.WorldToScreenPoint(Camera.main, coordinates);
+        float correctYPosition = (Screen.height - coordinates.y) - 100; 
+        popUpWindow = new Rect(coordinates.x - 350, correctYPosition,300, 300);
     }
 
     // Update is called once per frame
     public override void Update()
     {
-      if(isSelected && Input.GetMouseButtonDown(0))
+      if(wasClicked && Input.GetMouseButtonDown(0))
       {
          transform.GetChild(0).GetComponent<Renderer>().enabled = true;
-         isSelected = false;
+         isSelected = true;
+         wasClicked = false;
       }
-      else if(!isSelected && Input.GetMouseButtonDown(0))
+      else if(!wasClicked && Input.GetMouseButtonDown(0))
       {
           transform.GetChild(0).GetComponent<Renderer>().enabled = false;
+          isSelected = false;
       }
     }
 }
