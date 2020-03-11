0b223734ab22ef5624425dfe9d8e2701055824f3
Michael Seck <michaelseck7@gmail.com>
2020-03-11 17:46:58 -0500

    Organized files, added player object

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//we can attach this to an empty gameobject
+public abstract class Player : MonoBehaviour
+{
+    private int numPlayers = 2;
+    public int playerId; 
+    public bool isPlayerTurn; 
+    public GameObject[] ships; 
+    public int money;
+
+    //instantiate two players here
+    // Start is called before the first frame update
+    void Start()
+    {   
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
